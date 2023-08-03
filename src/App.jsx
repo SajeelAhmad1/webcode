7602cecc00385a7f4277efeb77fd64aa64bb185c
arpankumarde <academiaindiana@outlook.com>
2023-08-03 23:09:32 +0530

    navbar fixed

--- src/App.jsx
+++ src/App.jsx
@@ -20,40 +20,37 @@ import Quiz5 from './Components/Quizes/Quiz5'
 import Quiz6 from './Components/Quizes/Quiz6'
 import Quiz7 from './Components/Quizes/Quiz7'
 
-
-
-
 const App = () => {
   return (
-    
-    <><Header/><br /><br />
-    <Routes>
-      
-      <Route path='/tutorials' element={<Ch1/>}/>
-      <Route path="/tutorials/Chapter1" element={<Ch1 />} />
-      <Route path="/tutorials/Chapter2" element={<Ch2 />} />
-      <Route path="/tutorials/Chapter3" element={<Ch3 />} />
-      <Route path="/tutorials/Chapter4" element={<Ch4 />} />
-      <Route path="/tutorials/Chapter5" element={<Ch5 />} />
-      <Route path="/tutorials/Chapter6" element={<Ch6 />} />
-      <Route path="/tutorials/Chapter7" element={<Ch7 />} />
-      
-      <Route path='/' element={<Home/>} />
-      <Route path='/projects' element={<Projects/>}/>
-      <Route path="/cppatglance" element={<CppatGlance/>}/>
-      <Route path="/quizes" element={<Quiz1/>}/>
-      <Route path="/quizes/quiz1" element={<Quiz1/>}/>
-      <Route path="/quizes/quiz2" element={<Quiz2/>}/>
-      <Route path="/quizes/quiz3" element={<Quiz3/>}/>
-      <Route path="/quizes/quiz4" element={<Quiz4/>}/>
-      <Route path="/quizes/quiz5" element={<Quiz5/>}/>
-      <Route path="/quizes/quiz6" element={<Quiz6/>}/>
-      <Route path="/quizes/quiz7" element={<Quiz7/>}/>
-      
+    <>
+      <Header />
+
+      <Routes>
+        <Route path='/tutorials' element={<Ch1 />} />
+        <Route path="/tutorials/Chapter1" element={<Ch1 />} />
+        <Route path="/tutorials/Chapter2" element={<Ch2 />} />
+        <Route path="/tutorials/Chapter3" element={<Ch3 />} />
+        <Route path="/tutorials/Chapter4" element={<Ch4 />} />
+        <Route path="/tutorials/Chapter5" element={<Ch5 />} />
+        <Route path="/tutorials/Chapter6" element={<Ch6 />} />
+        <Route path="/tutorials/Chapter7" element={<Ch7 />} />
+
+        <Route path='/' element={<Home />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path="/cppatglance" element={<CppatGlance />} />
+        <Route path="/quizes" element={<Quiz1 />} />
+        <Route path="/quizes/quiz1" element={<Quiz1 />} />
+        <Route path="/quizes/quiz2" element={<Quiz2 />} />
+        <Route path="/quizes/quiz3" element={<Quiz3 />} />
+        <Route path="/quizes/quiz4" element={<Quiz4 />} />
+        <Route path="/quizes/quiz5" element={<Quiz5 />} />
+        <Route path="/quizes/quiz6" element={<Quiz6 />} />
+        <Route path="/quizes/quiz7" element={<Quiz7 />} />
+
       </Routes>
-      
-      
-  </>
+
+
+    </>
   )
 }
 export default App;
--- src/Components/Home/NewHeader.jsx
+++ src/Components/Home/NewHeader.jsx
@@ -3,73 +3,63 @@ import "./Home.css";
 import { Link } from "react-router-dom";
 
 const Header = () => {
-  const signin = () =>{
-    return(
+  const signin = () => {
+    return (
       <div class="ui placeholder segment">
-  <div class="ui two column very relaxed stackable grid">
-    <div class="column">
-      <div class="ui form">
-        <div class="field">
-          <label>Username</label>
-          <div class="ui left icon input">
-            <input type="text" placeholder="Username"/>
-            <i class="user icon"></i>
+        <div class="ui two column very relaxed stackable grid">
+          <div class="column">
+            <div class="ui form">
+              <div class="field">
+                <label>Username</label>
+                <div class="ui left icon input">
+                  <input type="text" placeholder="Username" />
+                  <i class="user icon"></i>
+                </div>
+              </div>
+              <div class="field">
+                <label>Password</label>
+                <div class="ui left icon input">
+                  <input type="password" />
+                  <i class="lock icon"></i>
+                </div>
+              </div>
+              <div class="ui blue submit button">Login</div>
+            </div>
+          </div>
+          <div class="middle aligned column">
+            <div class="ui big button">
+              <i class="signup icon"></i>
+              Sign Up
+            </div>
           </div>
         </div>
-        <div class="field">
-          <label>Password</label>
-          <div class="ui left icon input">
-            <input type="password"/>
-            <i class="lock icon"></i>
-          </div>
+        <div class="ui vertical divider">
+          Or
         </div>
-        <div class="ui blue submit button">Login</div>
       </div>
-    </div>
-    <div class="middle aligned column">
-      <div class="ui big button">
-        <i class="signup icon"></i>
-        Sign Up
-      </div>
-    </div>
-  </div>
-  <div class="ui vertical divider">
-    Or
-  </div>
-</div>
     )
   }
+
   return (
-    <>
-      <div className="nav ">
-        <div className="mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-          {" "}
-          <div className="logo"> 
-          <Link to='/'>CodeCrafter</Link></div>
-          <div className="first-header-bar text-white text-sm text-center sm:text-left text-xl"></div>
-          
-          
-          
-          <div className="header-bar text-white text-center sm:text-left text-xl">
-            <Link to="/tutorials/Chapter1">Tutorials</Link>
-          </div>
-          <div className="header-bar text-white text-center sm:text-left text-xl">
-            <Link to="/quizes/quiz1">Quizes</Link>
-          </div>
-          <div className="header-bar text-white text-center sm:text-left text-xl">
-            <Link to="/projects">Projects</Link>
-          </div>
-          <div className="header-bar text-white text-center sm:text-left text-xl">
-            <Link to="/cppatglance">C++ at Glance</Link>
-          </div>
-          {/* <div class="header-bar-btn">
-            <button class="ui button"
-            onClick={()=>signin}
-            >Signin</button>
-          </div> */}
+    <nav className="bg-[#222222] w-full fixed top-0 z-50 p-5 flex flex-col sm:flex-row gap-2 justify-between items-center">
+      <div className="text-white w-full flex justify-center sm:justify-normal">
+        <Link to='/' className="text-2xl font-bold">CodeCrafter</Link>
+      </div>
+      <div className="flex flex-row flex-wrap sm:flex-nowrap items-center justify-center sm:justify-end w-full gap-4">
+        <div className="text-white text-center sm:text-left text-xl">
+          <Link to="/tutorials/Chapter1">Tutorial</Link>
+        </div>
+        <div className="text-white text-center sm:text-left text-xl">
+          <Link to="/quizes/quiz1">Quiz</Link>
+        </div>
+        <div className="text-white text-center sm:text-left text-xl">
+          <Link to="/projects">Projects</Link>
+        </div>
+        <div className="text-white text-center sm:text-left text-xl">
+          <Link to="/cppatglance">C++ at Glance</Link>
         </div>
       </div>
-    </>
+    </nav>
   );
 };
 export default Header;
